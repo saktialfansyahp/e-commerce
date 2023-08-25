9890ac25b56973957d5d94cfcd23f681a6919983
saktialfansyahp <saktialfansyahp@gmail.com>
2023-08-25 18:14:14 +0700

    add Modals

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,91 +1,169 @@
 <template>
-  <html class="h-full bg-white">
-    <body class="h-full">
-      <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-      >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            class="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2
-            class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-          >
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div class="mt-10 space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
-          <!-- <form class="space-y-6"> -->
-          <div>
-            <label
-              for="email"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Email address</label
-            >
-            <div class="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                v-model="email"
-                autocomplete="email"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div class="flex items-center justify-between">
-              <label
-                for="password"
-                class="block text-sm font-medium leading-6 text-gray-900"
-                >Password</label
-              >
-              <div class="text-sm">
-                <a
-                  href="#"
-                  class="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >Forgot password?</a
-                >
-              </div>
-            </div>
-            <div class="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="current-password"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              @click="login"
-              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-          <!-- </form> -->
-          <p class="mt-10 text-center text-sm text-gray-500">
-            Not a member?
-            {{ " " }}
-            <a
-              href="#"
-              class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >Start a 14 day free trial</a
-            >
-          </p>
-        </div>
+  <body class="h-full">
+    <div
+      class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+    >
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          class="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2
+          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+        >
+          Sign in to your account
+        </h2>
       </div>
-    </body>
-  </html>
+
+      <div class="mt-10 space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <!-- <form class="space-y-6"> -->
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              v-model="email"
+              autocomplete="email"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
+            >
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >Forgot password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            @click="login"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+        <!-- </form> -->
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Not a member?
+          {{ " " }}
+          <a
+            href="#"
+            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >Start a 14 day free trial</a
+          >
+        </p>
+      </div>
+    </div>
+    <TransitionRoot as="template" :show="open">
+      <Dialog as="div" class="relative z-10" @close="open = false">
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          />
+        </TransitionChild>
+
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+          <div
+            class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
+          >
+            <TransitionChild
+              as="template"
+              enter="ease-out duration-300"
+              enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter-to="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leave-from="opacity-100 translate-y-0 sm:scale-100"
+              leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <DialogPanel
+                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+              >
+                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-center">
+                    <div class="mx-auto text-center sm:mt-0 sm:text-left">
+                      <DialogTitle
+                        as="h3"
+                        class="text-base font-semibold leading-6 text-gray-900"
+                        >Please fill out the form with the required
+                        information</DialogTitle
+                      >
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          Before proceeding, please take a moment to provide the
+                          necessary information in the form below. This
+                          information is crucial for the next steps
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+                >
+                  <button
+                    type="button"
+                    class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    @click="open = false"
+                    ref="cancelButtonRef"
+                  >
+                    Oke
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+  </body>
 </template>
+
+<script setup>
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+// import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+</script>
 
 <script>
 export default {
@@ -93,14 +171,20 @@ export default {
     return {
       role: "",
       email: "",
+      open: false,
     };
   },
   methods: {
     login() {
-      this.$store.commit("setRole", this.email);
-      this.$store.commit("setLoggedIn", true);
-      console.log(this.$store.state.role);
-      this.$router.push("/");
+      if (this.email === "admin" || this.email === "user") {
+        this.$store.commit("setRole", this.email);
+        this.$store.commit("setLoggedIn", true);
+        console.log(this.$store.state.role);
+        this.$router.push("/");
+      } else {
+        // Handle invalid email case
+        this.open = true;
+      }
     },
   },
 };
