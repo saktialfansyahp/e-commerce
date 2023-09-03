3544c5f496cd5cc17dcba028f9b355189cca257d
saktialfansyahp <saktialfansyahp@gmail.com>
2023-09-04 01:01:41 +0700

    setting modals

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -107,15 +107,15 @@
               enter="ease-out duration-300"
               enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               enter-to="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
+              leave="ease-in duration-300"
               leave-from="opacity-100 translate-y-0 sm:scale-100"
               leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <DialogPanel
-                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                class="fixed transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
               >
                 <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div class="sm:flex sm:items-center">
+                  <div class="flex sm:flex sm:items-center">
                     <div class="mx-auto text-center sm:mt-0 sm:text-left">
                       <DialogTitle
                         as="h3"
@@ -192,7 +192,8 @@ export default {
           this.$router.push("/");
         })
         .catch((error) => {
-          console.error(error);
+          console.error(error.response.data.message);
+          this.open = true;
         });
     },
     // async login() {
