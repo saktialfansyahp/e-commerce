93f3a691ddd2bb11e2d431f10c78f6904211393d
saktialfansyahp <saktialfansyahp@gmail.com>
2023-10-15 22:55:14 +0700

    add search

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -24,15 +24,16 @@
             </svg>
           </div>
           <input
+            v-model="searchQuery"
             type="text"
             id="table-search"
-            class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg sm:w-40 md:w-60 lg:w-80 bg-gray-50 py-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search for items"
           />
           <button
             @click="navigate"
             type="button"
-            class="mr-10 text-gray-900 bg-white border border-gray-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="text-gray-900 bg-white border border-gray-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -93,7 +94,7 @@
         </thead>
         <tbody>
           <tr
-            v-for="item in phone"
+            v-for="item in filteredProduct"
             :key="item.id"
             @click="detail(item.Product.id)"
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer"
@@ -292,6 +293,38 @@ export default {
   },
   created() {
     this.fetchData();
+  },
+  computed: {
+    filteredProduct() {
+      const searchTerm = this.searchQuery.toLowerCase();
+      let filter = this.phone
+        .filter((product) => {
+          const isMatchingName = product.Product.product_name
+            .toLowerCase()
+            .includes(searchTerm);
+          const isMatchingColor = product.Product.Color.color
+            .toLowerCase()
+            .includes(searchTerm);
+          const isMatchingCategory =
+            product.Product.Subcategory.Category.category
+              .toLowerCase()
+              .includes(searchTerm);
+          const isMatchingSub = product.Product.Subcategory.sub_category
+            .toLowerCase()
+            .includes(searchTerm);
+          return (
+            isMatchingName ||
+            isMatchingColor ||
+            isMatchingSub ||
+            isMatchingCategory
+          );
+        })
+        .sort((a, b) =>
+          a.Product.product_name.localeCompare(b.Product.product_name)
+        );
+
+      return filter;
+    },
   },
   methods: {
     detail(id) {
